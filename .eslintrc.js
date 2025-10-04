f0dd7e01101d58e7217f995f8a655b2bfb0f3376
Martin Kedmenec <martin.kedmenec@outlook.com>
2025-10-04 19:15:24 +0200

    Fix #409. Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
+  root: true,
   extends: ["expo", "eslint:recommended", "prettier"],
   plugins: ["prefer-arrow-functions"],
+  parser: "@typescript-eslint/parser",
   settings: {
     "import/resolver": {
       typescript: {
@@ -10,6 +12,50 @@ module.exports = {
   },
   rules: {
     "prefer-arrow-functions/prefer-arrow-functions": ["error"],
+    "no-restricted-imports": [
+      "error",
+      {
+        paths: [
+          {
+            name: "prop-types",
+            message: "Use the TypeScript type system instead.",
+          },
+          {
+            name: "react",
+            importNames: ["default"],
+            message:
+              "Don't default import React. It is unnecessary since React 17.",
+          },
+        ],
+        patterns: [
+          {
+            group: ["**/tailwind.config", "**/tailwind.config.js"],
+            message:
+              "Don't import tailwind.config.js at runtime. Use className with NativeWind or tokens (e.g. '@/theme/tokens').",
+          },
+          {
+            group: ["./", "../"],
+            message:
+              "Use absolute imports instead (e.g. '@/components/Button').",
+          },
+        ],
+      },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        format: ["strictCamelCase", "UPPER_CASE", "StrictPascalCase"],
+      },
+      {
+        selector: "typeLike",
+        format: ["StrictPascalCase"],
+      },
+      {
+        selector: "enumMember",
+        format: ["UPPER_CASE"],
+      },
+    ],
   },
   env: {
     browser: true,
