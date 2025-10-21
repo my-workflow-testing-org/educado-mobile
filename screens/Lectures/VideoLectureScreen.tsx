eaa1bd35b2f367a13b4c36d4450af6480df3b57b
Kristiyan Georgiev <2003kristiyan@gmail.com>
2025-10-21 12:21:48 +0200

    Fix type errors

--- screens/Lectures/VideoLectureScreen.tsx
+++ screens/Lectures/VideoLectureScreen.tsx
@@ -1,15 +1,14 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { View, TouchableOpacity, Alert, Text } from "react-native";
-import { Video, ResizeMode } from 'expo-av';
-import PropTypes from "prop-types";
+import { Video, ResizeMode } from "expo-av";
 import { Icon } from "@rneui/themed";
 import { useNavigation } from "@react-navigation/native";
 import { completeComponent, handleLastComponent } from "@/services/utils";
 import { getVideoURL } from "@/services/storage-service";
-import { Lecture} from "@/types/lecture";
+import { Lecture } from "@/types/lecture";
 import { Course } from "@/types/course";
 
-interface VideoLectureScreenProps {
+export interface VideoLectureScreenProps {
   lectureObject: Lecture;
   courseObject: Course;
   isLastSlide: boolean;
@@ -23,10 +22,10 @@ const VideoLectureScreen = ({
   isLastSlide,
   onContinue,
   handleStudyStreak,
-} : VideoLectureScreenProps)  => {
+}: VideoLectureScreenProps) => {
   const navigation = useNavigation();
   const [videoUrl, setVideoUrl] = useState<null | string>(null);
-  const [currentResolution, setCurrentResolution] = useState("1080");
+  const currentResolution = "1080";
 
   // Fetch video URL based on current resolution
   useEffect(() => {
@@ -34,15 +33,16 @@ const VideoLectureScreen = ({
       getVideoURL(lectureObject.content, currentResolution)
         .then((url) => {
           // Check if URL is a video
-          return fetch(url, { method: 'HEAD' }).then((response) => {
-            const contentType: string | null = response.headers.get('content-type');
-            if (!contentType || contentType.indexOf('video/') !== 0) {
+          return fetch(url, { method: "HEAD" }).then((response) => {
+            const contentType: string | null =
+              response.headers.get("content-type");
+            if (!contentType?.startsWith("video/")) {
               throw new Error("Invalid URL: " + url);
             }
             setVideoUrl(url);
           });
         })
-        .catch((error) => {
+        .catch((error: unknown) => {
           console.error(error);
           setVideoUrl(null);
         });
@@ -55,7 +55,7 @@ const VideoLectureScreen = ({
       try {
         handleStudyStreak();
         await completeComponent(lectureObject, courseObject.courseId, true);
-        handleLastComponent(lectureObject, courseObject, navigation);
+        await handleLastComponent(lectureObject, courseObject, navigation);
       } catch (error) {
         console.error("Error completing the course:", error);
         Alert.alert(
@@ -65,7 +65,7 @@ const VideoLectureScreen = ({
         );
       }
     } else {
-      if (onContinue && typeof onContinue === "function") {
+      if (typeof onContinue === "function") {
         onContinue(); // Advance to the next slide
       } else {
         console.warn("onContinue prop is not provided or not a function.");
@@ -77,23 +77,26 @@ const VideoLectureScreen = ({
     <View className="flex-1 pt-20">
       {/* Course info */}
       <View className="mt-5 flex-col items-center">
-        <Text className="font-sans text-base text-projectGray">
+        <Text className="text-textDisabledGrayscale text-body-regular">
           Nome do curso: {courseObject.title}
         </Text>
-        <Text className="font-sans-bold text-lg text-projectBlack">
+        <Text className="text-textTitleGrayscale text-body-bold">
           {lectureObject.title}
         </Text>
       </View>
 
       {/* Video Player */}
       <View className="flex-1 p-8">
-        <View className="rounded-lg overflow-hidden" style={{ aspectRatio: 7/10 }}>
+        <View
+          className="w-full overflow-hidden rounded-xl"
+          style={{ aspectRatio: 7 / 10 }}
+        >
           {videoUrl ? (
             <Video
               source={{ uri: videoUrl }}
               shouldPlay={true}
               resizeMode={ResizeMode.COVER}
-              style={{ width: '100%', height: '100%' }}
+              style={{ width: "100%", height: "100%" } as const}
             />
           ) : (
             <View className="flex-1 items-center justify-center">
@@ -106,33 +109,25 @@ const VideoLectureScreen = ({
       {/* Continue button */}
       <View className="w-100 mb-8 px-6">
         <TouchableOpacity
-          className="flex-row items-center justify-center rounded-medium bg-primary_custom px-10 py-4"
-          onPress={handleContinuePress}
+          className="flex-row items-center justify-center rounded-medium bg-surfaceDefaultCyan px-10 py-4"
+          onPress={() => void handleContinuePress()}
         >
           <View className="flex-row items-center">
-            <Text className="font-sans-bold text-center text-body text-projectWhite">
+            <Text className="text-center text-surfaceSubtleGrayscale text-body-bold">
               Continuar
             </Text>
             <Icon
+              className="ml-8"
               name="chevron-right"
               type="material"
               size={24}
               color="white"
-              style={{ marginLeft: 8 }}
             />
           </View>
         </TouchableOpacity>
       </View>
     </View>
   );
-};
-
-VideoLectureScreen.propTypes = {
-  lectureObject: PropTypes.object.isRequired,
-  courseObject: PropTypes.object.isRequired,
-  isLastSlide: PropTypes.bool.isRequired,
-  onContinue: PropTypes.func.isRequired,
-  handleStudyStreak: PropTypes.func.isRequired,
 };
 
 export default VideoLectureScreen;
